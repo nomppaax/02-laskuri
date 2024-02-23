2413da257fd10d058b1478ec2e18cf71211dd49d
Noomi <kykkanenn@gmail.com>
2024-02-23 14:35:12 +0200

    10- this katoaa, kun pyydetään esim.sekunnin kuluttua

--- tiedosto.js
+++ tiedosto.js
@@ -1,17 +1,9 @@
 const arto = {
   name: "Arto Hellas",
-  age: 35,
-  education: "Filosofian tohtori",
   greet: function () {
     console.log("hello, my name is", this.name);
   },
-
-  doAddition: function (a, b) {
-    console.log(a + b);
-  },
 };
 
-arto.doAddition(1, 4); // tulostuu 5
-
-const referenceToAddition = arto.doAddition;
-referenceToAddition(10, 15); // tulostuu 25
+setTimeout(arto.greet.bind(arto), 1000);
+// sekunnin päästä tulostuu hello, my name is Arto Hellas
